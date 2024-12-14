3487572454fc0e0e7e4f7883b4ffe370f3b7dad3
joepasss@gentoo <farwes1180@gmail.com>
2024-12-14 21:02:16 +0000

    config.h init & add fullscreen mode

--- dwm.c
+++ dwm.c
@@ -199,6 +199,7 @@ static void sendmon(Client *c, Monitor *m);
 static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
+static void fullscreen(const Arg *arg);
 static void setlayout(const Arg *arg);
 static void setmfact(const Arg *arg);
 static void setup(void);
@@ -1506,6 +1507,19 @@ setfullscreen(Client *c, int fullscreen)
 	}
 }
 
+Layout *last_layout;
+void
+fullscreen(const Arg *arg)
+{
+  if (selmon->showbar) {
+    for (last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
+    setlayout(&((Arg) { .v = &layouts[2] }));
+  } else {
+    setlayout(&((Arg) { .v = last_layout  }));
+  }
+  togglebar(arg);
+}
+
 void
 setlayout(const Arg *arg)
 {
@@ -2152,6 +2166,15 @@ main(int argc, char *argv[])
 		die("dwm: cannot open display");
 	checkotherwm();
 	setup();
+
+  if (fork() == 0) {
+    setsid();
+    char *fcitxcmd[] = { "fcitx5", NULL };
+    execvp(fcitxcmd[0], fcitxcmd);
+    fprintf(stderr, "dwm: execvp %s failed\n", fcitxcmd[0]);
+    exit(EXIT_FAILURE);
+  }
+
 #ifdef __OpenBSD__
 	if (pledge("stdio rpath proc exec", NULL) == -1)
 		die("pledge");
